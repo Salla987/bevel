15b9ba3e24c0185c0b567f6877ea2cb545390e00
Salla987 <salla987salla@gmail.com>
2025-04-17 12:14:41 +0300

    [ci skip] Pushing deployment files for namespace, service accounts and clusterrolebinding
    Signed-off-by: Salla987 <salla987salla@gmail.com>

--- run.sh
+++ run.sh
@@ -13,10 +13,10 @@ echo "Adding env variables..."
 export PATH=/root/bin:$PATH
 
 # Path to k8s config file
-export KUBECONFIG=/home/bevel/build/config
+export KUBECONFIG=/home/ali/blockchain/hyperledger-bevel/bevel/project/bevel/bevel/build/config
 
 echo "Validatin network yaml"
-ajv validate -s /home/bevel/platforms/network-schema.json -d /home/bevel/build/network.yaml 
+ajv validate -s /home/ali/blockchain/hyperledger-bevel/bevel/project/bevel/bevel/platforms/network-schema.json -d /home/ali/blockchain/hyperledger-bevel/bevel/project/bevel/bevel/build/network.yaml 
 
 echo "Running the playbook..."
-exec ansible-playbook -vv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+exec ansible-playbook -vv /home/ali/blockchain/hyperledger-bevel/bevel/project/bevel/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/ali/blockchain/hyperledger-bevel/bevel/project/bevel/bevel/platforms/shared/inventory/ -e "@//home/ali/blockchain/hyperledger-bevel/bevel/project/bevel/bevel/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass
